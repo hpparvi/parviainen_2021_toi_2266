60315e68bead57d21cf2bf6c77bae38a82b8f3ed
Hannu Parviainen <hpparvi@gmail.com>
2022-06-01 20:37:57 +0100

    Added free transit centre times.

--- toi_2266.py
+++ toi_2266.py
@@ -2,12 +2,14 @@ import sys
 from pathlib import Path
 from typing import Optional
 
-from numpy import diff, sqrt, arange, array, ndarray, inf, atleast_2d, zeros, sum, median, where
+import pandas as pd
+from numpy import diff, sqrt, arange, array, ndarray, inf, atleast_2d, zeros, sum, median, where, repeat, unique
 from pytransit import sdss_g, sdss_r, sdss_i, sdss_z, RoadRunnerModel
 from pytransit.contamination import Instrument, SMContamination
 from pytransit.lpf.cntlpf import contaminate
 from pytransit.lpf.tess.tgclpf import BaseTGCLPF
-from pytransit.orbits import as_from_rhop, i_from_ba
+from pytransit.orbits import as_from_rhop, i_from_ba, epoch
+from pytransit.param import GParameter, UniformPrior as UP, NormalPrior as NP
 from uncertainties import ufloat
 
 sys.path.append('..')
@@ -57,6 +59,7 @@ class LPF(BaseTGCLPF):
         self.m2_passbands = m2_passbands
         tm = RoadRunnerModel('power-2-pm', small_planet_limit=0.005, parallel=True)
         super().__init__(name, use_ldtk, tm=tm)
+        self.tm.epids[:] = self.epids
 
     def read_data(self):
         times_t, fluxes_t, pbs_t, wns_t, ins_t, piis_t = read_tess_data(zero_epoch, period,
@@ -69,6 +72,10 @@ class LPF(BaseTGCLPF):
 
         times = times_t + times_m2 + times_l + times_h
         fluxes = fluxes_t + fluxes_m2 + fluxes_l + fluxes_h
+
+        self.epochs = epoch(array([median(a) for a in times]), zero_epoch.n, period.n)
+        self.epids = pd.Categorical(self.epochs).codes
+        self.nepochs = self.epids[-1] + 1
 
         pbs = pbs_t + pbs_m2 + pbs_l + pbs_h
         wns = wns_t + wns_m2 + wns_l + wns_h
@@ -85,17 +92,43 @@ class LPF(BaseTGCLPF):
 
         return times, fluxes, pbnames, pbs, wns, covs
 
+
+    def _init_p_orbit(self):
+        """Orbit parameter initialisation.
+        """
+        porbit = [
+            GParameter('p', 'period', 'd', NP(1.0, 1e-5), (0, inf)),
+            GParameter('rho', 'stellar_density', 'g/cm^3', UP(0.1, 25.0), (0, inf)),
+            GParameter('b', 'impact_parameter', 'R_s', UP(0.0, 1.0), (0, 1))]
+        self.ps.add_global_block('orbit', porbit)
+
+        ptc = [GParameter(f'tc_{i}', f'transit_center_{i}', '-', NP(0.0, 0.1), (-inf, inf)) for i in range(self.nepochs)]
+        self.ps.add_global_block('tc', ptc)
+        self._pid_tc = repeat(self.ps.blocks[-1].start, self.nlc)
+        self._start_tc = self.ps.blocks[-1].start
+        self._sl_tc = self.ps.blocks[-1].slice
+
     def _init_instrument(self):
         """Set up the instrument and contamination model."""
         self.instrument = Instrument('example', [sdss_g, sdss_r, sdss_i, sdss_z])
         self.cm = SMContamination(self.instrument, "i'")
-        self.add_prior(lambda pv: where(pv[:, 4] < pv[:, 5], 0, -inf))
-        self.add_prior(lambda pv: where(pv[:, 8] < pv[:, 5], 0, -inf))
+        self.add_prior(lambda pv: where(pv[:, self._i_k2a] < pv[:, self._i_k2t], 0, -inf))
+        self.add_prior(lambda pv: where(pv[:, self._i_k2at] < pv[:, self._i_k2t], 0, -inf))
 
     def _post_initialisation(self):
+
+        self._i_k2a = self.ps.find_pid('k2_app')
+        self._i_k2t = self.ps.find_pid('k2_true')
+        self._i_k2at = self.ps.find_pid('k2_app_tess')
+        self._i_teffh = self.ps.find_pid('teff_h')
+        self._i_teffc = self.ps.find_pid('teff_c')
+
         if self.use_opencl:
             self.tm = self.tm.to_opencl()
-        self.set_prior('tc', 'NP', zero_epoch.n, zero_epoch.s)
+
+        for i,e in enumerate(unique(self.epochs)):
+            self.set_prior(f'tc_{i}', 'NP', zero_epoch.n + period.n*e, 0.01)
+
         self.set_prior('p', 'NP', period.n, period.s)
         self.set_prior('rho', 'UP', 20, 35)
         self.set_prior('k2_app', 'UP', 0.02 ** 2, 0.08 ** 2)
@@ -116,14 +149,14 @@ class LPF(BaseTGCLPF):
     def transit_model(self, pvp):
         pvp = atleast_2d(pvp)
         cnt = zeros((pvp.shape[0], self.npb))
-        zero_epoch = pvp[:,0] - self._tref
-        period = pvp[:,1]
-        smaxis = as_from_rhop(pvp[:, 2], period)
-        inclination  = i_from_ba(pvp[:, 3], smaxis)
-        radius_ratio = sqrt(pvp[:,5:6])
+        zero_epoch = pvp[:, self._sl_tc] - self._tref
+        period = pvp[:,0]
+        smaxis = as_from_rhop(pvp[:, 1], period)
+        inclination  = i_from_ba(pvp[:, 2], smaxis)
+        radius_ratio = sqrt(pvp[:, self._i_k2t : self._i_k2t+1])
         ldc = pvp[:, self._sl_ld].reshape([-1, self.npb, 2])
         flux = self.tm.evaluate(radius_ratio, ldc, zero_epoch, period, smaxis, inclination)
-        cnt[:, 0] = 1 - pvp[:, 8] / pvp[:, 5]
-        cnref = 1. - pvp[:, 4] / pvp[:, 5]
-        cnt[:, 1:] = self.cm.contamination(cnref, pvp[:, 6], pvp[:, 7])
+        cnt[:, 0] = 1 - pvp[:, self._i_k2at] / pvp[:, self._i_k2t]
+        cnref = 1. - pvp[:, self._i_k2a] / pvp[:, self._i_k2t]
+        cnt[:, 1:] = self.cm.contamination(cnref, pvp[:, self._i_teffh], pvp[:, self._i_teffc])
         return contaminate(flux, cnt, self.lcids, self.pbids)
